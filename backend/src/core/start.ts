af242241f88a591335d41ab5d323fe8c1d9166a3
S B Rakesh Rath <work.sbrakeshrath@gmail.com>
2025-07-18 01:03:09 +0530

    Bug Fix

--- backend/src/core/start.ts
+++ backend/src/core/start.ts
@@ -32,7 +32,7 @@ io.on("connection", (socket) => {
   AddPoints(socket);
 });
 
-httpServer.listen(PORT, () => {
+httpServer.listen(Number(PORT), '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
